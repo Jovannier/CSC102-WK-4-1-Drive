ae8e8cec13973eba074b9f746709522ff5b2c041
Jovannier <147970973+Jovannier@users.noreply.github.com>
2023-11-09 11:03:32 -0600

    Add files via upload

---
+++ code.js
@@ -0,0 +1,21 @@
+function startFun(){
+    console.log("startFun() started");
+    document.getElementById("startButton").disabled = true;
+    document.getElementById("stopButton").disabled = false;
+    //start marquee to move
+    document.getElementById("myMarquee").start();
+
+}
+
+function stopFun(){
+    //log for debugging purposes
+    console.log("stopFun() started");
+    //turn off stop button
+    document.getElementById("stopButton").disabled = true;
+    //turn on start button
+    document.getElementById("startButton").disabled = false;
+    //stop marquee from moving
+        document.getElementById("myMarquee").stop();
+}
+
+
